2f3b2da6f67c835c78ec1380a1f2f04623a89adf
AnonPrixorPH <62997139+AnonPrixorPH@users.noreply.github.com>
2023-02-08 23:43:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ pip install pycryptodome;
 pip3 install colorama;
 pip3 install httpx;
 unzip PRIXOR-KILLER.zip;
-pip3 install asciimatic;
+pip install asciimatics;
 unzip YOLANDA.zip;
 chmod +x *;
 sudo apt install screen;
